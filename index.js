1a9f7c4bc35e816dbf3fd752d97fb411d6adf1c3
danielytuk <cueisdi@gmail.com>
2021-02-21 22:23:40 +0000

    Signed-off-by: danielytuk

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
 const axios = require('axios');
-const instagram = async (username) => {
-	
-	if(!username) return console.error("Username is undefined.");
-    const options = { method: 'GET', url: `https://www.instagram.com/${username}/?__a=1` };
-    
-	try {
-		const fetch = await axios(options);
-		const data = fetch.data;
-		
-		const user = data.graphql.user;
-		const media = user.edge_owner_to_timeline_media.edges;
-		if(!media) return console.error("No media found on that profile.");
-	} catch(error) {
-		return console.error("Something went wrong...")
-	}
+const instagram = async(username) => {
 
-    return {
-        fullName: user.full_name,
-        username: user.username,
-        id: user.id,
-        avatar: user.profile_pic_url_hd,
-        biography: user.biography,
-        followers: user.edge_followed_by.count,
-        isPrivate: user.is_private,
-        isVerified: user.is_verified,
-        latestPost: media.map(edge => {
-            return {
-                url: `https://www.instagram.com/p/${edge.node.shortcode}/`,
-                imageUrl: edge.node.display_url,
-                caption: edge.node.edge_media_to_caption.edges[0] ? edge.node.edge_media_to_caption.edges[0].node.text : null,
-                likesCount: edge.node.edge_liked_by.count,
-                commentsCount: edge.node.edge_media_to_comment.count,
-                timestamp: edge.node.taken_at_timestamp,
-            }
-        })
+    if (!username) return console.error("Username is undefined.");
+    const options = {
+        method: 'GET',
+        url: `https://www.instagram.com/${username}/?__a=1`
+    };
+
+    try {
+        const fetch = await axios(options);
+        const data = fetch.data;
+
+        const user = data.graphql.user;
+        const media = user.edge_owner_to_timeline_media.edges;
+        if (!media) return console.error("No media found on that profile.");
+
+        return {
+            fullName: user.full_name,
+            username: user.username,
+            id: user.id,
+            avatar: user.profile_pic_url_hd,
+            biography: user.biography,
+            followers: user.edge_followed_by.count,
+            isPrivate: user.is_private,
+            isVerified: user.is_verified,
+            latestPost: media.map(edge => {
+                return {
+                    url: `https://www.instagram.com/p/${edge.node.shortcode}/`,
+                    imageUrl: edge.node.display_url,
+                    caption: edge.node.edge_media_to_caption.edges[0] ? edge.node.edge_media_to_caption.edges[0].node.text : null,
+                    likesCount: edge.node.edge_liked_by.count,
+                    commentsCount: edge.node.edge_media_to_comment.count,
+                    timestamp: edge.node.taken_at_timestamp,
+                }
+            })
+        }
+    } catch (error) {
+        return console.error("Something went wrong...")
     }
 }
 
